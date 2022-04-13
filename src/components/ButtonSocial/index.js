7117241df8b789953e03ffe80d1c50c3c3d9514d
drinobre <dri_nobre@hotmail.com>
2022-04-13 11:55:37 +0100

    added links to the social buttons

--- src/components/ButtonSocial/index.js
+++ src/components/ButtonSocial/index.js
@@ -3,13 +3,23 @@
 import React from "react";
 import { Button, Icon } from "semantic-ui-react";
 
-const ButtonSocial = () => (
+const ButtonSocial = ({ link }) => (
   <div className="mobile-hidden">
-    <Button color="purple">Portfolio</Button>
-    <Button color="linkedin">
+    <Button
+      href="https://nobre-lawrence.vercel.app/"
+      target="_blank"
+      color="purple"
+    >
+      Portfolio
+    </Button>
+    <Button
+      href="https://www.linkedin.com/in/adriana-nobre-lawrence/"
+      target="_blank"
+      color="linkedin"
+    >
       <Icon name="linkedin" /> Linkedin
     </Button>
-    <Button color="github">
+    <Button href="https://github.com/drinobre" target="_blank" color="github">
       <Icon name="github" /> Github
     </Button>
   </div>
