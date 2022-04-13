3921494cb0c3936a11d71a9a756a68c0922d3080
drinobre <dri_nobre@hotmail.com>
2022-04-13 10:05:35 +0100

    refactor navbar component and css

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,23 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <header>
-      <nav class="navbar-container">
-        {/* <div class="navbar-left">
-          <h2>Welcome to Drink API</h2>
-        </div> */}
-        <div class="navbar-right">
-          <span class="navbar-link">
-            <Link to="cocktail/">Home</Link>
-          </span>
-          <span class="navbar-link">
-            <Link to="cocktail/surprise_me">Surprise me</Link>
-          </span>
-          {/* <span class="navbar-link">
-            <Link to="/search_drink">Search drink</Link>
-          </span> */}
-        </div>
-      </nav>
-    </header>
+    <div class="navbar-container">
+      <div class="navbar-link">
+        <Link to="cocktail/">Home</Link>
+      </div>
+      <div class="navbar-link">
+        <Link to="cocktail/surprise_me">Surprise me</Link>
+      </div>
+    </div>
   );
 }
